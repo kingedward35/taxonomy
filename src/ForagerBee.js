42dae70b106ecf43a92cb5fde08869a34d977dfc
Verlon Smith <verlonsmith@Verlons-MacBook-Air.local>
2015-08-24 17:55:48 -0600

    Passes all tests

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
 var ForagerBee = function() {
+  this.age = 10;
+  this.job = 'find pollen';
+  this.color = 'yellow';
+  this.food = 'jelly';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Grub.prototype);
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+};
+
+ForagerBee.prototype.constructor = ForagerBee;
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
 var HoneyMakerBee = function() {
+  this.age = 10;
+  this.job = 'make honey';
+  this.color = 'yellow';
+  this.food = 'jelly';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Grub.prototype);
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
